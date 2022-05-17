2494cd89043d856e8ec0cec4062f902ea4bdfddc
Profundis <1mailanton@gmail.com>
2022-05-17 15:36:59 +0200

    change Dependencies.kt

--- buildSrc/src/main/kotlin/utils/Versions.kt
+++ buildSrc/src/main/kotlin/utils/Versions.kt
@@ -1,6 +1,16 @@
 package utils
 
 object Versions {
+    const val room = "2.3.0"
+    const val dagger = "2.40.5"
+    const val koin = "3.1.4"
+    const val glide = "4.13.0"
+    const val timber = "5.0.1"
+    const val serialization = "1.3.0"
+    const val mapStruct = "1.4.2.Final"
+    const val ktor = "1.6.3"
+    const val okHttp = "4.9.1"
+    const val retrofit = "2.9.0"
     const val gradle = "7.1.2"
     const val kotlin = "1.6.20"
 
@@ -13,4 +23,6 @@ object Versions {
     const val coreKtx = "1.7.0"
 
     const val navigation = "2.3.0-alpha01"
+    const val lifecycle = "2.5.0-rc01"
+    const val coroutines = "1.6.1"
 }
